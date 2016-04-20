8848d04d70ae0e75ee64772f425afa0518202a1b
vaibhavmule <vaibhavmule135@gmail.com>
2016-04-20 20:52:45 +0530

    add arrow function to tests and update run scripts on package.json

--- test/test.js
+++ test/test.js
@@ -2,11 +2,11 @@ import { expect } from 'chai';
 import Status from '../src/index.js';
 
 describe("Status", () => {
-  it("should return 200", function () {
+  it("should return 200", () => {
     expect(Status.http200Ok).to.equal(200);
   });
 
-  it("should return 500", function () {
+  it("should return 500", () => {
     expect(Status.http500InternalServerError).to.equal(500);
   });
 });
