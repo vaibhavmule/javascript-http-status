b5e0bf9d607f6fc8202bd9746a06fd62f6f89709
vaibhavmule <vaibhavmule135@gmail.com>
2016-04-20 21:11:44 +0530

    remote semi-colon rules from eslint

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
 import { expect } from 'chai';
 import Status from '../src/index.js';
 
-describe("Status", () => {
-  it("should return 200", () => {
+describe('Status', () => {
+  it('should return 200', () => {
     expect(Status.http200Ok).to.equal(200);
   });
 
-  it("should return 500", () => {
+  it('should return 500', () => {
     expect(Status.http500InternalServerError).to.equal(500);
   });
 });
