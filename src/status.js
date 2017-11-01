ca01bde594a2bd2ee22eb2f6a727b6a60e77ac84
Vaibhav Mule <vaibhavmule135@gmail.com>
2017-11-01 12:55:03 +0530

    mention of non standard status code

--- src/status.js
+++ src/status.js
@@ -41,6 +41,7 @@ const Status = {
   http428PreconditionRequired: 428,
   http429TooManyRequests: 429,
   http431RequestHeaderFieldsTooLarge: 431,
+  http444NoResponse: 444, // Non Standard status code for nginx https://httpstatuses.com/444
   http451UnavailableForLegalReasons: 451,
   http500InternalServerError: 500,
   http501NotImplemented: 501,
@@ -49,7 +50,6 @@ const Status = {
   http504GatewayTimeout: 504,
   http505HttpVersionNotSupported: 505,
   http511NetworkAuthenticationRequired: 511,
-  http444NoResponse: 444,
 };
 
 export default Status;
